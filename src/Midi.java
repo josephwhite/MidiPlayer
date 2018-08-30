72207a325a2e906f2ea3543a1243cf30466ae1e7
Stay-Puff <damasta21th@aol.com>
2018-08-29 23:04:50 -0400

    PlayMidi Comments

--- src/Midi.java
+++ src/Midi.java
@@ -4,7 +4,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Scanner;
 import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
@@ -12,47 +11,68 @@ import javax.sound.midi.Sequence;
 import javax.sound.midi.Sequencer;
 
 public class Midi {
-	
-	private Sequencer sequencer;
-	private boolean isPlaying;
-	private long currentPosition;
-	
+
+	Sequencer sequencer;
+	boolean isPlaying;
+	long currentPosition;
+	float currentSongBPM;
+
 	public Midi() {
 		try {
 			// Obtains the default Sequencer connected to a default device.
 			sequencer = MidiSystem.getSequencer();
-		} catch (MidiUnavailableException e) {
-			// TODO Auto-generated catch block
+		}
+		catch (MidiUnavailableException e) {
 			e.printStackTrace();
 		}
 		isPlaying = false;
 		currentPosition = 0;
 	}
-	
-	public void PlayMidi(String fileName) throws IOException, InvalidMidiDataException, MidiUnavailableException {
-    	// Opens the device, indicating that it should now acquire any
-        // system resources it requires and become operational.
-		sequencer.open();
-		
-		// create a stream from a file
-        InputStream is = new BufferedInputStream(new FileInputStream(new File(fileName)));
-        
-        // Sets the current sequence on which the sequencer operates.
-        // The stream must point to MIDI file data.
-        sequencer.setSequence(is);
 
-        // Starts playback of the MIDI data in the currently loaded sequence.
-        sequencer.setMicrosecondPosition(currentPosition);
-        sequencer.start();
-        isPlaying = true;
+	/**
+	 * Plays a local MIDI file.
+	 * @param fileName
+	 * @throws IOException
+	 * @throws InvalidMidiDataException
+	 * @throws MidiUnavailableException
+	 */
+	public void PlayMidi(String fileName) throws IOException, InvalidMidiDataException, MidiUnavailableException {
+		// Opens the device, indicating that it should now acquire any
+		// system resources it requires and become operational.
+		sequencer.open();
+
+		// create a stream from a file
+		InputStream is = new BufferedInputStream(new FileInputStream(new File(fileName)));
+
+		// Sets the current sequence on which the sequencer operates.
+		// The stream must point to MIDI file data.
+		sequencer.setSequence(is);
+
+		// Starts playback of the MIDI data in the currently loaded sequence.
+		sequencer.setMicrosecondPosition(currentPosition);
+		currentSongBPM = sequencer.getTempoInBPM();
+		sequencer.start();
+		isPlaying = true;
+		if (sequencer.getTickPosition() == sequencer.getTickLength()){
+			sequencer.stop();
+			isPlaying = false;
+		}
+
 	}
-	
-	public void StopMidi() {
+
+	public void PauseMidi() {
 		sequencer.stop();
 		currentPosition = sequencer.getMicrosecondPosition();
 		isPlaying = false;
 	}
-	
-	public boolean getIsPlaying() { return isPlaying; }
-	
+
+	public boolean getIsPlaying() {
+		return isPlaying;
+	}
+	public float getTempo(){
+		return sequencer.getTempoInBPM();
+	}
+	public void setTempo(float newTempo) {
+		sequencer.setTempoInBPM(newTempo);
+	}
 }
--- src/PlayMidiAudio.java
+++ src/PlayMidiAudio.java
@@ -1,21 +1,33 @@
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class PlayMidiAudio {
-	static String MIDI_FILE = "src/resources/Wild_Pokemon_Battle_RB.mid";
+
+	static String MIDI_FILE = "Wild_Pokemon_Battle_RB.mid";
 	static Scanner scanner;
-	
+
 	public static void main(String[] args) throws Exception {
+
 		Midi midi = new Midi();
 		scanner = new Scanner(System.in);
 		String input = "";
+
 		do {
-			input = scanner.nextLine().toLowerCase();
-			if (input.equals("play") && !midi.getIsPlaying()) {
+			input = scanner.next().toLowerCase();
+			if (input.equals("play") && !midi.isPlaying) {
 				midi.PlayMidi(MIDI_FILE);
-			} else if (input.equals("pause") && midi.getIsPlaying()) {
-				midi.StopMidi();
+			}
+			else if (input.equals("pause") && midi.isPlaying) {
+				midi.PauseMidi();
+			}
+			else if (input.equals("current tempo")) {
+				JOptionPane.showMessageDialog(null, "The current tempo is " + midi.getTempo());
+			}
+			else if (input.equals("tempo")) {
+				int numberInput = scanner.nextInt();
+				midi.setTempo(numberInput);
 			}
 		} while (!input.equals("quit"));
 	}
-	
 }
